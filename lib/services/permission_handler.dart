c0181baaf64f7f608af025f61f3dc08200361c11
Harshana Rathnayaka <huratnayaka@gmail.com>
2024-11-07 18:11:09 +0530

    fixed the notification permission issue on Android 13

--- lib/services/permission_handler.dart
+++ lib/services/permission_handler.dart
@@ -1,20 +1,32 @@
 import 'dart:developer';
+import 'dart:io';
 
 import 'package:permission_handler/permission_handler.dart';
+/*
+* Notifications are enabled by default in Android API below 33
+* But we have to request permission if its above API 33
+*/
 
 class AppRequest {
   static Future<bool> notificationPermission() async {
-    PermissionStatus request = await Permission.notification.request();
+    // permission request is need on API level 33 or above
+    if (Platform.isAndroid && (await Permission.notification.isDenied)) {
+      PermissionStatus request = await Permission.notification.request();
 
-    if (request.isGranted) {
-      log('Notification permission granted');
+      if (request.isGranted) {
+        log('Notification permission granted');
+        return true;
+      } else if (request.isDenied) {
+        log('Notification permission denied');
+        return false;
+      } else if (request.isPermanentlyDenied) {
+        log('Notification permission permanently denied');
+        return false;
+      }
+    } else {
+      // if iOS or below API level 33
+      log('Notification permission not required or already granted');
       return true;
-    } else if (request.isDenied) {
-      log('Notification permission denied');
-      return false;
-    } else if (request.isPermanentlyDenied) {
-      log('Notification permission permanently denied');
-      return false;
     }
     return false;
   }
