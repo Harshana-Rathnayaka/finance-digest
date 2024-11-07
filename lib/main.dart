5e61a942856e70cd5700fa74b4f0766dc43e4a35
Harshana Rathnayaka <huratnayaka@gmail.com>
2024-11-07 17:19:02 +0530

    removed dead code

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:finance_digest/utils/router.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
